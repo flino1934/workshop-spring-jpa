2386b1613f5241e8c62f099923ba01647b39a570
flino1934 <f.lino1934@hotmail.com>
2022-11-11 15:33:19 -0300

    Criando entidade Order e as associações entre usurio e pedido
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/course/model/entities/User.java
+++ src/main/java/com/lino/course/model/entities/User.java
@@ -1,12 +1,15 @@
 package com.lino.course.model.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")//o mapeamento esta mostrando o nome que esta sendo usado na outra classe -> private User client;
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -73,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override

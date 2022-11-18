efcd167f05f5e7b2e94446e524dd2295901ff586
flino1934 <f.lino1934@hotmail.com>
2022-11-18 11:34:43 -0300

    Criando tratamento de exceção do update
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/course/model/service/UserService.java
+++ src/main/java/com/lino/course/model/service/UserService.java
@@ -3,6 +3,8 @@ package com.lino.course.model.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -37,28 +39,32 @@ public class UserService {
 
 	public void delete(Long id) {
 		try {
-			
+
 			userRepository.deleteById(id);
 
 		} catch (EmptyResultDataAccessException e) {
-			
+
 			throw new ResourceNotFoundException(id);
-		
-		}catch (DataIntegrityViolationException e) {
-			
+
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DatabaseException(e.getMessage());
-		
+
 		}
-		
 
 	}
 
 	public User update(Long id, User obj) {
 
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
 
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
